975568aa73f7ac089886839aefc924bcae92152b
aaronsapa1 <aarn.sapa@gmail.com>
2015-08-19 13:16:29 -0500

    tarea0

---
+++ exp/server.R
@@ -0,0 +1,33 @@
+
+# This is the server logic for a Shiny web application.
+# You can find out more about building applications with Shiny here:
+#
+# http://shiny.rstudio.com
+#
+
+library(shiny)
+
+LCG<-function(nsim,M=(2^32),a=1664525,b=1013904223,x0=1){
+  X = c(x0,numeric(nsim-1))
+  for(i in 1:(nsim-1)){
+    X[i+1]<-(a*X[i]+b) %% M
+  }
+  return (X/M)
+}
+
+
+shinyServer(function(input, output) {
+
+  output$distPlot <- renderPlot({
+
+    # generate bins based on input$bins from ui.R
+    x    <- LCG(input$nsim)
+    l <- input$lambda
+    exp <- sapply(x,function(y){-1*(log(1-y)/l)})
+
+    # draw the histogram with the specified number of bins
+    hist(exp, col = 'darkgray', border = 'white')
+
+  })
+
+})
---
+++ exp/ui.R
@@ -0,0 +1,31 @@
+
+# This is the user-interface definition of a Shiny web application.
+# You can find out more about building applications with Shiny here:
+#
+# http://shiny.rstudio.com
+#
+
+library(shiny)
+
+shinyUI(fluidPage(
+
+  # Application title
+  titlePanel("Exponential Distribuiton"),
+
+  # Sidebar with a slider input for number of bins
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("nsim",
+                  "Num sim:",
+                  min = 1,
+                  max = 5000,
+                  value = 300),
+      numericInput("lambda", "lambda:", value=10,min=1)
+    ),
+
+    # Show a plot of the generated distribution
+    mainPanel(
+      plotOutput("distPlot")
+    )
+  )
+))
